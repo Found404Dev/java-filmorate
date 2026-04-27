01e11ef4435fd9f73c46cdad86a8a065dc33ab72
Yury Bodyakov <xGin1990x@yandex.ru>
2026-04-27 17:41:56 +0300

    fix: пробелы

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -16,24 +16,28 @@ public class Film {
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
     public void setDescription(String description) {
         this.description = description;
     }
+
     public LocalDate getReleaseDate() {
         return releaseDate;
     }
     public void setReleaseDate(LocalDate releaseDate) {
         this.releaseDate = releaseDate;
     }
+
     public Integer getDuration() {
         return duration;
     }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -16,24 +16,28 @@ public class User {
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getLogin() {
         return login;
     }
     public void setLogin(String login) {
         this.login = login;
     }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public LocalDate getBirthday() {
         return birthday;
     }
